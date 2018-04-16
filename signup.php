2d3e106f9743551ee9cc49497403639f9cca2cab
RealArcadex <realarcadex@gmail.com>
2018-04-16 20:10:06 +1000

    font update

--- signup.php
+++ signup.php
@@ -6,9 +6,9 @@
     <div class="main-wrapper">
         <h2>Signup</h2>
         <form class="signup-form" action="includes/signup.inc.php" method="POST">
-            <input type="text" name="first" placeholder="Firstname">
-            <input type="text" name="last" placeholder="Lastname">
-            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="first" placeholder="First Name">
+            <input type="text" name="last" placeholder="Last Name">
+            <input type="text" name="email" placeholder="E-Mail">
             <input type="text" name="uid" placeholder="Username">
             <input type="password" name="pwd" placeholder="Password">
             <button type="submit" name="submit">Sign up</button>
