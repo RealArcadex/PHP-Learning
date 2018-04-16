19e7a8d962cccfbbed2b3ec01b86f420b659a220
RealArcadex <realarcadex@gmail.com>
2018-04-16 20:06:45 +1000

    Progressed on footer.php
    info.php added

---
+++ info.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'header.php';
+?>
+
+<section class="main-container">
+    <div class="main-wrapper">
+        <h2>Info</h2>
+    </div>
+    <body>
+        <br/>
+        <p><center>Welcome to my website!<br/></center><center>This page is in development... so there isn't much here! Check back soon!</center></p>
+    </body>
+</section>
+
+<?php
+    include_once 'footer.php';
+?>
