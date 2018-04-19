37872d71757063633cf6d3c4e1c204fed564143d
RealArcadex <realarcadex@gmail.com>
2018-04-19 20:24:18 +1000

    temp twitter widget

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
             }
 			include_once 'sidebar.php';
         ?>
-		<center><font color="red">Whoops! You caught us whilst we haven't even figured out what we're doing!</font></center>
-		</body>
+		<center><font color="red">Whoops! You caught us whilst we haven't even figured out what we're doing!</font></center></br>
+		<p align="right"><a class="twitter-timeline" height="1000px" width="350px" href="https://twitter.com/ArcadexFR?ref_src=twsrc%5Etfw">Tweets by ArcadexFR</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p align="right">
+        </body>
 	</div>
 </section>
 
