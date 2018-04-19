c2d7263402be039fdb1b1aa6555c5ca92b37c837
RealArcadex <realarcadex@gmail.com>
2018-04-19 19:34:21 +1000

    Changed to fit sidebar

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 <section class="main-container">
     <div class="main-wrapper">
         <h2>Home</h2>
-        <h2>Info</h2>
+		<body>
         <?php
             if (isset($_SESSION['u_id'])) {
                 echo "You are logged in!";
             }
+			include_once 'sidebar.php';
         ?>
-    </div>
+		<center><font color="red">Whoops! You caught us whilst we haven't even figured out what we're doing!</font></center>
+		</body>
+	</div>
 </section>
 
 <?php
--- info.php
+++ info.php
@@ -7,8 +7,11 @@
         <h2>Info</h2>
     </div>
     <body>
+		<?php
+			include_once 'sidebar.php';
+		?>
         <br/>
-        <p><center>Welcome to my website!- But looks like we're messing some stuff...<br/></center><center><img src="http://www.jkp.com/jkpblog/wp-content/uploads/2014/08/whoops-Adele-Devine-image.jpg" alt="Looks like we are missing something..." height="250" width="250" /></center></p>
+        <p><center>Welcome to my website!- But looks like we're messing some stuff...<br/></br></center><center><img src="http://www.jkp.com/jkpblog/wp-content/uploads/2014/08/whoops-Adele-Devine-image.jpg" alt="Looks like we are missing something..." height="250" width="250" /></center></p>
     </body>
 </section>
 
