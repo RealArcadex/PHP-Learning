0c20d4c444139813b1128c9278f8d0eb0fe3a6aa
RealArcadex <realarcadex@gmail.com>
2018-04-22 21:13:28 +1000

    news and updates

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <section class="main-container">
     <div class="main-wrapper">
-        <h2>Home</h2>
+        <h2>Home</h2></br>
 		<body>
         <?php
             if (isset($_SESSION['u_id'])) {
@@ -12,8 +12,27 @@
             }
 			include_once 'sidebar.php';
         ?>
-		<center><font color="red">Whoops! You caught us whilst we haven't even figured out what we're doing!</font></center></br>
-		<p align="right"><a class="twitter-timeline" height="1000px" width="350px" href="https://twitter.com/ArcadexFR?ref_src=twsrc%5Etfw">Tweets by ArcadexFR</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p align="right">
+		<div class="updates">
+            </br>
+            <div class="header-up">
+            <b><font size="6"><center>News &amp; Updates</center></font></b>
+            </div>
+            </br>
+            <div class="update-april">
+                <p>Created a staff page ⁃ <b>21/04/18</b></p>‣</br>
+                <p>Restrictions have been added! ⁃ <b>21/04/18</b></p>‣<br/>
+                <p>New button styles! ⁃ <b>21/04/18</b></p>
+            </div>
+            <div class="april-title">
+                <center>April 2018</center>
+            </div>
+            <div class="update-may">
+                <p>Unfinished ⁃ <b>dd/05/18</b></p>‣</br>
+                <p>Unfinished ⁃ <b>dd/05/18</b></p>‣<br/>
+                <p>Unfinished ⁃ <b>dd/05/18</b></p>
+            </div>
+        </div>
+		<!-- <p align="right"><a class="twitter-timeline" height="1000px" width="350px" href="https://twitter.com/ArcadexFR?ref_src=twsrc%5Etfw">Tweets by ArcadexFR</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p align="right"> -->
         </body>
 	</div>
 </section>
