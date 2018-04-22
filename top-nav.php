75853dc97a567a115dc5d8120d8eaf5c0faee904
RealArcadex <realarcadex@gmail.com>
2018-04-22 19:18:12 +1000

    new files

--- top-nav.php
+++ top-nav.php
@@ -1,6 +1,14 @@
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">Info</a>
-  <a href="#contact">Discord</a>
-  <a href="#about">Contact</a>
-</div>
+<html>
+    <head>
+        <title>top-navigation</title>
+        <link rel="stylesheet" type="text/css" href="t.css">
+    </head>
+    <body>
+    <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">Info</a>
+    <a href="#contact">Discord</a>
+    <a href="#about">Contact</a>
+    </div>
+    </body>
+</html>
